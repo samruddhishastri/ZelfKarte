064d20a857e2bc1dd2872935eb27c0ce7ff53d96
S-Shastri <samruddhishastri20@google.com>
2020-06-19 16:57:11 +0530

    Added content from video 7

--- summerup/forms.py
+++ summerup/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from summerup.models import User
@@ -21,3 +23,15 @@ class LoginForm(FlaskForm):
 	password = PasswordField('Password', validators=[DataRequired()])
 	remember = BooleanField('Remember Me')
 	submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+	username = StringField('Name', validators=[DataRequired(), Length(min=2, max=100)])
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
+	submit = SubmitField('Update')
+
+	def validate_email(self, email):
+		if email.data != current_user.email:
+			user = User.query.filter_by(email=email.data).first()
+			if user:
+				raise ValidationError('This email is already registered. Please use another email to sign in.')
--- summerup/routes.py
+++ summerup/routes.py
@@ -1,7 +1,10 @@
+import os
+import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from summerup import app, db, bcrypt
 from summerup.models import User
-from summerup.forms import SignupForm, LoginForm
+from summerup.forms import SignupForm, LoginForm, UpdateAccountForm
 from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route("/")
@@ -14,14 +17,11 @@ def home():
 	return render_template('home.html')
 
 @app.route('/about')
-@login_required
 def about():
 	return render_template('about.html', title='About')
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-	if current_user.is_authenticated:
-		return redirect(url_for('home'))
 	form = SignupForm()
 	if form.validate_on_submit():
 		hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -29,13 +29,12 @@ def signup():
 		db.session.add(user)
 		db.session.commit()
 		flash(f'Your account has been created.', 'success')
+		login_user(user)
 		return redirect(url_for('home'))
 	return render_template('signup.html', title='Signup', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	if current_user.is_authenticated:
-		return redirect(url_for('home'))
 	form = LoginForm()
 	if form.validate_on_submit():
 		user = User.query.filter_by(email=form.email.data).first()
@@ -61,7 +60,35 @@ def logout():
 	logout_user()
 	return redirect(url_for('launch'))
 
-@app.route('/account')
+
+def save_picture(form_picture):
+	random_hex = secrets.token_hex(8)
+	_, f_ext = os.path.splitext(form_picture.filename)
+	picture_fn = random_hex + f_ext
+	picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+	
+	output_size = (125,125)
+	i = Image.open(form_picture)
+	i.thumbnail(output_size)
+	i.save(picture_path)
+	return picture_fn
+
+
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
-	return render_template('account.html', title='Account')
+	form = UpdateAccountForm()
+	if form.validate_on_submit():
+		if form.picture.data:
+			picture_file = save_picture(form.picture.data)
+			current_user.image_file = picture_file
+		current_user.username = form.username.data
+		current_user.email = form.email.data
+		db.session.commit()
+		flash('Your Account is Updated', 'success')
+		return redirect(url_for('account'))
+	elif request.method == 'GET':
+		form.username.data = current_user.username
+		form.email.data = current_user.email
+	image_file = url_for('static', filename="profile_pics/" + current_user.image_file)
+	return render_template('account.html', title='Account', image_file = image_file, form = form)
